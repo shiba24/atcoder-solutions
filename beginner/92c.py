e5fc41cc809399c29fafbfa4db4a5477457269bd
Shintaro Shiba <shiba.shintaro@gmail.com>
2018-03-25 23:16:32 +0900

    92c

---
+++ beginner/92c.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+N = input()
+A = np.array(map(int, raw_input().split()))
+
+
+A = np.append(np.append([0], A), [0])
+total_sum = 0
+for i in range(0, len(A) - 1):
+    total_sum += np.abs(A[i + 1] - A[i])
+
+for i in range(1, len(A) - 1):
+    partial_sum = total_sum - np.abs(A[i + 1] - A[i]) - np.abs(A[i] - A[i - 1]) + np.abs(A[i + 1] - A[i - 1])
+    print partial_sum
+
+
+
+N = input()
+A = map(int, raw_input().split())
+
+
+A = [0] + A + [0]
+total_sum = 0
+for i in range(0, len(A) - 1):
+    total_sum += abs(A[i + 1] - A[i])
+
+for i in range(1, len(A) - 1):
+    partial_sum = total_sum - abs(A[i + 1] - A[i]) - abs(A[i] - A[i - 1]) + abs(A[i + 1] - A[i - 1])
+    print partial_sum
+
